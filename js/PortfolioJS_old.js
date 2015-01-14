d703af521c26c609c0bd8cac73113875646a1aae
Patric Sterrantino <flytoe@gmail.com>
2015-01-14 17:33:50 +0100

    #07

--- js/PortfolioJS_old.js
+++ js/PortfolioJS_old.js
@@ -16,16 +16,18 @@ var ascensorInstance = $('#Work').data('ascensor');
 			$(".links-to-floor li").click(function(event, index) {
 				ascensorInstance.scrollToFloor($(this).index());
 			});
+
 			$(".links-to-floor li:eq("+ ascensor.data("current-floor") +")").addClass("selected");
 			ascensor.on("scrollStart", function(event, floor){
 				$(".links-to-floor li").removeClass("selected");
 				$(".links-to-floor li:eq("+floor.to+")").addClass("selected");
 			});
-			$(".prev").click(function() {
+			
+            $(".prev").on('touchstart click', function() {
 				ascensorInstance.prev();
-			});
-				
-			$(".next").click(function() {
+            });
+			
+            $(".next").on('touchstart click', function() {
 				ascensorInstance.next();
 			});
 				
