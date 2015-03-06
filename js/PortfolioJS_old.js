51768725dda69187966526787fc15592e0910680
flytoe <flytoe@gmail.com>
2015-03-06 11:03:52 +0100

    #17

--- js/PortfolioJS_old.js
+++ js/PortfolioJS_old.js
@@ -9,58 +9,27 @@ var ascensor = $('#Work').ascensor({
     loop: true,
     childType: 'article'
 });
-	
+
 
 $(document).ready(function () {
-   if (window.matchMedia('(max-width: 680px)').matches) {
-       $("#menu-0 a").text("About Patric Sterrantino");
-       $("#menu-1 a").text("Conception and Prototyping");
-       $("#menu-2 a").text("User Interface Design");
-       $("#menu-3 a").text("Graphic and Motion Design");
-       $("#menu-4 a").text("Mobile App and Service Design");
-       $("#menu-5 a").text("Installations and Exhibitions");
-       $("#menu-6 a").text("Contact");
-    } else {
+    if (window.matchMedia('(max-width: 680px)').matches) {
+        $("#menu-0 a").text("About Patric Sterrantino");
+        $("#menu-1 a").text("Conception and Prototyping");
+        $("#menu-2 a").text("User Interface Design");
+        $("#menu-3 a").text("Graphic and Motion Design");
+        $("#menu-4 a").text("Mobile App and Service Design");
+        $("#menu-5 a").text("Installations and Exhibitions");
+        $("#menu-6 a").text("Contact");
     }
 });
 
-var ascensorInstance = $('#Work').data('ascensor');		
-			$(".links-to-floor li").click(function(event, index) {
-				ascensorInstance.scrollToFloor($(this).index());
-                
-                if (window.matchMedia('(max-width: 680px)').matches) {
-                   toggleMenu();
-                } else {
-                    
-                }
-			});
-
-			$(".links-to-floor li:eq("+ ascensor.data("current-floor") +")").addClass("selected");
-			ascensor.on("scrollStart", function(event, floor){
-				$(".links-to-floor li").removeClass("selected");
-				$(".links-to-floor li:eq("+floor.to+")").addClass("selected");
-			});
-			
-            $(".prev").on('click', function() {
-				ascensorInstance.prev();
-            });
-			
-            $(".next").on('click', function() {
-				ascensorInstance.next();
-			});
-
-
-$(".show-menu").on('click', function() {
-    toggleMenu();
-});
-
-var toggleMenu = function() {
+var toggleMenu = function () {
     $("nav").slideToggle({
         easing: 'easeInOutCubic',
         duration: 250
     });
     $(".show-menu").toggleClass("openIt");
-    $( ".global-header" ).toggleClass("fullscreen", 500);
+    $(".global-header").toggleClass("fullscreen", 500);
     $(".floor").fadeToggle({
         easing: 'easeInOutCubic',
         duration: 250
@@ -70,3 +39,31 @@ var toggleMenu = function() {
         duration: 250
     });
 };
+
+var ascensorInstance = $('#Work').data('ascensor');
+$(".links-to-floor li").click(function (event, index) {
+    ascensorInstance.scrollToFloor($(this).index());
+
+    if (window.matchMedia('(max-width: 680px)').matches) {
+        toggleMenu();
+    }
+});
+
+$(".links-to-floor li:eq(" + ascensor.data("current-floor") + ")").addClass("selected");
+ascensor.on("scrollStart", function (event, floor) {
+    $(".links-to-floor li").removeClass("selected");
+    $(".links-to-floor li:eq(" + floor.to + ")").addClass("selected");
+});
+
+$(".prev").on('click', function () {
+    ascensorInstance.prev();
+});
+
+$(".next").on('click', function () {
+    ascensorInstance.next();
+});
+
+
+$(".show-menu").on('click', function () {
+    toggleMenu();
+});
